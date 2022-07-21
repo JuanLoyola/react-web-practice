d9b4c5cd88e230f76c087e5fab07fedf083e0baf
Loyola <juanjoloyola@hotmail.com>
2022-07-21 13:33:25 -0300

    mid project

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import Navbar  from './components/Navbar'
+import Hero  from './components/Hero'
+import About  from './components/About'
+import Support  from './components/Support'
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <Hero />
+      <About />
+      <Support />
+    </>
+  )
+}
+
+export default App
